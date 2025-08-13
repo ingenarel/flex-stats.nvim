020ed3517112cfd54ee6f046dd93874a590223cb
ingenarel <ingenarelitems@gmail.com>
2025-08-13 16:34:38 +0600

    (refactor): break logic into more functions

--- lua/flex-stats/init.lua
+++ lua/flex-stats/init.lua
@@ -1,24 +1,40 @@
 local m = {}
-m.fileTimes = {}
-function m.setup()
-    vim.api.nvim_create_autocmd("InsertEnter", {
-        callback = function()
-            local filetype = vim.opt.filetype:get()
-            if type(m.fileTimes[filetype]) ~= "table" then
-                m.fileTimes[filetype] = { totalTime = 0 }
-            end
-            m.fileTimes[filetype].lastInsertEnter = os.time()
-        end,
-    })
 
+m.fileTimes = {}
+
+function m.startInsertTime()
+    local filetype = vim.opt.filetype:get()
+    m.fileTimes[filetype].lastInsertEnter = os.time()
+end
+
+function m.endInsertTime()
+    local filetype = vim.opt.filetype:get()
+    m.fileTimes[filetype].totalTime = m.fileTimes[filetype].totalTime
+        + os.time()
+        - m.fileTimes[filetype].lastInsertEnter
+    m.fileTimes[filetype].lastInsertEnter = nil
+end
+
+function m.filetypeSetup()
+    local filetype = vim.opt.filetype:get()
+    if type(m.fileTimes[filetype]) ~= "table" then
+        m.fileTimes[filetype] = {}
+    end
+    if type(m.fileTimes[filetype].totalTime) ~= "number" then
+        m.fileTimes[filetype].totalTime = 0
+    end
+end
+
+function m.setup()
+    vim.api.nvim_create_autocmd("FileType", {
+        callback = m.filetypeSetup,
+    })
+    vim.api.nvim_create_autocmd("InsertEnter", {
+        callback = m.startInsertTime,
+    })
     vim.api.nvim_create_autocmd("InsertLeave", {
-        callback = function()
-            local filetype = vim.opt.filetype:get()
-            m.fileTimes[filetype].totalTime = m.fileTimes[filetype].totalTime
-                + os.time()
-                - m.fileTimes[filetype].lastInsertEnter
-            m.fileTimes[filetype].lastInsertEnter = nil
-        end,
+        callback = m.endInsertTime,
     })
 end
+
 return m
