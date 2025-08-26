fe42aac4db5ec0cb3883f6794807e5dafc8e8dd6
ingenarel <ingenarelitems@gmail.com>
2025-08-26 17:15:05 +0600

    (feat): add a command to show the stats

--- lua/flex-stats/init.lua
+++ lua/flex-stats/init.lua
@@ -6,6 +6,9 @@ local db = require("flex-stats.core.db")
 function m.setup()
     m.database = db.readDataBase()
     require("flex-stats.core.autocmds")
+    vim.api.nvim_create_user_command("Flex", function()
+        m.showStats()
+    end, {})
 end
 
 function m.showStats()
